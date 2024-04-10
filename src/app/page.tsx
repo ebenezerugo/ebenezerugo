aa5396502a14373762d5dc55cbfd46c687e7a319
ebenezerugo <ugo.ebenezer@cloudax.io>
2024-04-10 01:00:26 +0100

    chore(): joke

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,10 @@ export default function Home() {
           priority
         />
       </div>
-
+      <div>
+        <h2 className="text-lg font-medium">Tommy Beg your husband, no dey form hard girl.</h2>
+        <p>Shey you never tire?</p>
+      </div>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
